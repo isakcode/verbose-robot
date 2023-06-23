2a68636c6bc8c272309216e4d83f478fcae54bdc
Isaksend <64757132+isakcode@users.noreply.github.com>
2023-06-24 02:01:13 +0600

    Add files via upload

---
+++ src/Label.js
@@ -0,0 +1,10 @@
+import './App.css';
+export const Label = () => {
+    return(
+        <div className='LabelBox'>
+            <h1 className="LabelName">CALCULATOR</h1>
+            <h3>My first pet project in React</h3>
+        </div>
+        
+    )
+}
